d5cc2545fb582df32e5ed649d61b8f69729c56fc
José Gaspar Sánchez García <jg.sanchezgarcia@edu.gva.es>
2023-12-30 17:24:57 +0100

    No funciona bien cuando se lanza con compose.yml

--- my-selenium-web/src/index.js
+++ my-selenium-web/src/index.js
@@ -2,7 +2,7 @@
 const express=require('express');
 const app=express();
 const PORT=8080
-app.use(express.static('src/public'));
+app.use(express.static('www/public'));
 
 app.get('/',(req, res) => {
     res.render("index.js");
